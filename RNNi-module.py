f0004c53550980bfdeea69c61733a71363d9b8d9
therealjjj77 <j.johnson.bbt@gmail.com>
2020-12-11 13:33:17 +0700

    Update RNNi-module.py

--- RNNi-module.py
+++ RNNi-module.py
@@ -22,9 +22,6 @@ n = [np.int_, cv2.resize, np.isscalar, np.vstack, np.zeros, np.append, cv2.getRo
      cv2.warpAffine]
 id = image[0:fs[2]:, 0:fs[3]:]
 seed()
-X=[]
-y=[]
-label=1
 # number of looks per image
 while z < fs[5]:
 
@@ -33,7 +30,8 @@ while z < fs[5]:
     br = n[0]([b[0] * 255, b[1] * 255])
     i = 0
     fi = 0
-    rot = n[0]((fs[6] + 1) - random() * (fs[6] + 1) * 2)
+    rot = n[0]((random()*2-1) * (fs[6]+1))
+    print(rot)
     rotm = n[6]((fs[0] / 2, fs[0] / 2), rot, 1)
     while i < fs[1]:
         p = [n[0]((fs[2] - fs[0] * 2 ** i) / 2 + cf[0]), n[0]((fs[2] + fs[0] * 2 ** i) / 2 + cf[0]),
@@ -50,18 +48,19 @@ while z < fs[5]:
             fi = n[3]((fi, j))
         if i == (fs[1] - 1):
             # random adjust brightness/contrast
-            bt = n[0](random() * fs[7] - fs[7] / 2)
-            ct = n[0](random() * fs[8] - fs[8] / 2)
+            br=[random()*2-1,random()*2-1]
+            bt = n[0](br[0] * fs[7])
+            ct = n[0](br[1] * fs[8])
             fi = n[0](fi * (ct / 127 + 1) - ct + bt)
             # add final row with coordinates
             k = n[4]((fs[4] * fs[0] ** 2 - 2, 1))
             k = n[5](k, br)
-            fi = n[3]((fi, k))
+            fi = n[0](n[3]((fi, k)))
         i += 1
     image = fi
     # And append it and a label to the lists
-    X.append(image)
-    y.append(label)
+    #X.append(image)
+    #y.append(label)
     z += 1
 
 print(image)
